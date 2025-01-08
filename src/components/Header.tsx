6f044d2a4ae81f8f9bbfadf7aef9c7e67e7d70db
CHI THAI <21313683@students.ltu.edu.au>
2025-01-09 02:25:01 +1100

    Header & Navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { HomeIcon } from "lucide-react"
+import { File, HomeIcon, UserRound } from "lucide-react"
 import { NavButton } from "@/components/NavButton"
+import Link from "next/link"
 
 
 export function Header() {
@@ -7,10 +8,13 @@ export function Header() {
         <header className="animate_slide bg-background h-12 p-2 border-b sticky top-0 z-90">
             <div className="flex h-8 items-center justify-between w-full ">
                 <div className="flex items-center gap-2">
-                    <NavButton href="/home" label="Home" icon={HomeIcon}></NavButton>
+                    <NavButton href="/home" label="Home" icon={HomeIcon} />
+                    <Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home" />
+                    <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">Computer Repair shop</h1>
                 </div>
                 <div className="flex items-center">
-
+                    <NavButton href="/tickets" label="Tickets" icon={File} />
+                    <NavButton href="/customers" label="Customers" icon={UserRound} />
                 </div>
             </div>
         </header>
